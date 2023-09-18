8aed59eeecbd88ac647ae9fd86c45d2637e7ece0
Your Name <you@example.com>
2023-09-18 17:32:28 -0400

    assertEqual updated

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("123", "123");   //Comparing identical strings
+assertEqual("111", "222");   //Comparing non-identical strings
+assertEqual(1000, 1000);     //Comparing identical numbers
+assertEqual(2222, 3333);     //Comparing non-identical numbers
