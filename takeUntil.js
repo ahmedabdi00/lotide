c8bc555a6cbb294eac84497ebf34cd0e2c803d9e
Ahmed Abdi <a.abdi11965@gmail.com>
2023-09-25 11:03:08 -0400

    split function

---
+++ takeUntil.js
@@ -0,0 +1,53 @@
+function eqArrays(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.error(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+}
+
+const takeUntil = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      break;
+    }
+  }
+  return results;
+};
+
+// Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+const expectedResults1 = [1, 2, 5, 7, 2];
+assertArraysEqual(results1, expectedResults1); // Should pass
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+const expectedResults2 = ["I've", "been", "to", "Hollywood"];
+assertArraysEqual(results2, expectedResults2); // Should pass
